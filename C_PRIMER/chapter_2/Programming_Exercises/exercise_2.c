6fa703d09818e96a5f2b52e0342806b25ce95943
wong dawn <cqdsl18623169065@163.com>
2020-06-20 12:13:24 +0800

    EXERCISE_2

---
+++ C_PRIMER/chapter_2/Programming_Exercises/exercise_2.c
@@ -0,0 +1,9 @@
+/*2# Write a program to print your name and address.*/
+#include<stdio.h>
+int main(void)
+{
+    printf("Name:wexdda\n.Address:xx市xx区xx街道xx小区xx栋.\n");
+    //printf("Name:wexdda.\n");
+    //printf("Address:xx市xx区xx街道xx小区xx栋.\n");
+    return 0;
+}
