78105e1dba96f5d8aa8c89e5a3bec59180697504
wong dawn <cqdsl18623169065@163.com>
2020-07-07 10:35:25 +0800

    EXCEED(OVERFLOW)

--- C_PRIMER/chapter_3/toobig.c
+++ C_PRIMER/chapter_3/toobig.c
@@ -14,7 +14,15 @@ int main(void)
     return 0;
 }
 /*The result:
-            2147483647 -2147483648 -2147483647
+            2147483647 c -2147483647
             4294967295 0 1
 */
+/*
+The unsigned integer j is acring like a car's odometer.When it reaches its maximun value,it starts over at the beginning,
+The integer i acts similarly.The main difference is that the unsigned int variable j,like an odometer,begins at 0;
+but the integer variable i begins at -2147483648.Notice that you are not informed that i exceeded(overflow) its maximum
+value.
+The behavior describe here is mandated by the rules of C for unsigned type.The standard doesn't define how signed types
+should behave.The behavior shown here is typical,but you could encounter something different.
 
+*/
